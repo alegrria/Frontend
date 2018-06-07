635548f2c432435d1576552f4fcdb9e5af35d584
Dende <alegrria@gmail.com>
2018-06-07 10:03:50 +0200

    changes made upon review

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -5,7 +5,7 @@
  */
 $(function() {
 
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', function() {  // Passes when the feeds are loaded; when their URLs and names exist and are not empty.  
 
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
@@ -15,19 +15,19 @@ $(function() {
         it('have non-empty URLs', function(){
             for (feed in allFeeds) {
                 expect(allFeeds[feed].url).toBeDefined();
-                expect(allFeeds[feed].url).toContain('http://');
+                expect(allFeeds[feed].url.length).not.toBe(0);
             }
         }); 
 
         it('have non-empty names', function(){
             for (feed in allFeeds) {
                 expect(allFeeds[feed].name).toBeDefined();
-                expect(allFeeds[feed].name).not.toBeNull();
+                expect(allFeeds[feed].name.length).not.toBe(0);
             }
         }); 
     });
 
-    describe('The menu', function() {
+    describe('The menu', function() { // Passes, when the menu is hidden at the first load of the page and appears on click
         beforeEach(function(){
             el = document.querySelector('body');
         });
@@ -46,7 +46,7 @@ $(function() {
         });  
     });       
 
-    describe('Initial Entries', function() {
+    describe('Initial Entries', function() { // Passes, when a feed contains at least one entry
         beforeEach(function (done) {
             loadFeed(0, done); 
         });
@@ -59,13 +59,17 @@ $(function() {
 
     });
 
-    describe('New Feed Selection', function() {
-        beforeEach(function (done) {
-            loadFeed(3, done);
+    describe('New Feed Selection', function() { // Passes, when feed content changes upon loading another feed
+        beforeEach((done)=> {
+            loadFeed(0, function() {           
+                loadFeed(1, function() {
+                    done(); 
         });
+    });
+});
 
         it('should change the content', function(done){
-            expect($('.feed')[0].children[0].href).toContain('linear-digressions');
+            expect($('.feed')[0].children[0].href).toContain('https://www.youtube.com/watch?v=tvLF7zllsv0');
             done();
         }); 
     });     
